a1692d59caf9118bce1ab124b0ce435748c64458
mohammadk <mohammadkarrom@gmail.com>
2024-04-03 10:15:21 +0300

    test

--- Desktop/Al_Dandashi/src/user/urls.py
+++ Desktop/Al_Dandashi/src/user/urls.py
@@ -1,8 +1,7 @@
-from django.contrib import admin
-from django.urls import path, include
-from user import views
+from django.urls import path
+
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.user_login, name='login'),
+    path("", views.index, name="index"),
 ]
--- Desktop/Al_Dandashi/src/user/views.py
+++ Desktop/Al_Dandashi/src/user/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-def user_login(request):
-    return render(request, 'user/login.html', {'title': 'Home'})
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
